acf27823fc95967fd8b67f442e6043dcd390a36b
Kevin-DeBruyne <70618739+Kevin-DeBruyne@users.noreply.github.com>
2022-05-29 00:35:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Home from './Home.js';
+
+
+
+const App = () => {
+  
+  return (
+    <div>
+        <Home/>
+      </div>
+    
+  );
+}
+
+export default App;
