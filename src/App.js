73816668d567b8818b988901aff1dd01d07512c3
Kevin-DeBruyne <70618739+Kevin-DeBruyne@users.noreply.github.com>
2022-05-30 15:35:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Home from './Home.js';
+import Nav from './Nav.js';
+import React from 'react';
+// import { BrowserRouter as Router, Routes, Route}
+//     from 'react-router-dom';
+
+
+const App = () => {
+  
+  return (
+    <Nav/>
+    
+);
+}
+
+export default App;
---
+++ src/Nav.js
@@ -0,0 +1,28 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Container, Navbar, Row, Col } from "react-bootstrap";
+import Main from "./Main.js";
+import background from "./pexels-polina-zimmerman-3747468.jpg";
+
+
+const Nav = () => {
+  const navStyle = {
+    backgroundColor: "rgb(0,0,0,0.5)",
+  };
+ 
+
+  return (
+   
+          <Navbar fluid variant="dark" expand="lg" style={navStyle} >
+            <Container>
+            <Navbar.Brand href="#home">
+              <h1 style={{ color: "#82cff1" }}>LibRar</h1>
+              {/* <button onClick={func()}>admin</button> */}
+            </Navbar.Brand>
+            <Navbar.Toggle />
+            </Container>  
+          </Navbar>
+       
+  );
+};
+export default Nav;
